40eda8f1d2cc8920d005ef36c66bb7ccc929584e
Rich Leland <rich@richleland.com>
2010-08-01 20:38:43 -0400

    Added org-mode

--- leland/modes.el
+++ leland/modes.el
@@ -2,3 +2,11 @@
 (require 'ido)
 (ido-mode t)
 (setq ido-enable-flex-matching t)
+
+;; org-mode
+(setq load-path (cons "~/.emacs.d/vendor/org-mode/lisp" load-path))
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(add-hook 'org-mode-hook 'turn-on-font-lock)
