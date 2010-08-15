7de7ba1356f0d39beeb6d6108a5bc31cbf8c95db
Rich Leland <rich@richleland.com>
2010-08-15 19:57:12 -0400

    Tweaks to org mode config

--- leland/modes.el
+++ leland/modes.el
@@ -3,10 +3,14 @@
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 
-;; org-mode
+;; load org-mode
 (setq load-path (cons "~/.emacs.d/vendor/org-mode/lisp" load-path))
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 (add-hook 'org-mode-hook 'turn-on-font-lock)
+
+;; customize org-mode
+(setq org-todo-keywords
+  '((sequence "TODO" "FEEDBACK" "VERIFY" "|" "DONE" "DELEGATED")))
